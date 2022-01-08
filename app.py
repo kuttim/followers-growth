0394e7a406a18729e1b9aec2cd344e52011f26fc
Mikk Küttim <kuttim.mikk@gmail.com>
2022-01-08 21:41:00 +0200

    make code formatted

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ import plotly.express as px
 
 import csv
 
-
 # Map information from csv file to our figure.
+
 usernames = []
 followers_old = []
 followers_now = []
 
 # Map old csv to object
+
 with open('data/2016.csv', 'r') as f:
     reader = csv.reader(f)
     for row in reader:
@@ -23,28 +24,19 @@ with open('data/2022.csv', 'r') as f:
     for row in reader:
         followers_now.append(row[1])
 
-
 fig = go.Figure()
-fig.add_trace(go.Bar(
-    x=usernames,
-    y=followers_old,
-    name='2016',
-    marker_color='lightsalmon'
-))
-fig.add_trace(go.Bar(
-    x=usernames,
-    y=followers_now,
-    name='2022',
-    marker_color='indianred',
-))
-
+fig.add_trace(go.Bar(x=usernames, y=followers_old, name='2016',
+              marker_color='lightsalmon'))
+fig.add_trace(go.Bar(x=usernames, y=followers_now, name='2022',
+              marker_color='indianred'))
 
 fig.update_layout(
     autotypenumbers='convert types',
-
     barmode='group',
-    title="Top 25 instagram accounts growth from 2016 to 2022",
-    xaxis_title="Usernames",
-    yaxis_title="Followers (in millions)",
-    legend_title="Year", xaxis_tickangle=-50)
+    title='Top 25 instagram accounts growth from 2016 to 2022',
+    xaxis_title='Usernames',
+    yaxis_title='Followers (in millions)',
+    legend_title='Year',
+    xaxis_tickangle=-50,
+    )
 fig.show()
